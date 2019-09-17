f8370d8dbbaca0bd21a8301143765f4d61e31e2a
22karchetan <22karchetan246@gmail.com>
2019-09-17 12:38:28 +0530

    initial project laravel setup

---
+++ laravel/resources/views/Chetan_view.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=100%, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CHetan View</title>
+</head>
+<body>
+    <h1 style="text-align:center;">CHetan new View</h1>
+    
+</body>
+</html>
